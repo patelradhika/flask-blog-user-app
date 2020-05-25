0c99efce3a42c300e3d8bed3dc9ed8108a127548
Radhika Patel <radz@Radhikas-MacBook-Air.local>
2020-05-25 15:05:17 +0530

    Setup completed

--- radzblog/__init__.py
+++ radzblog/__init__.py
@@ -4,6 +4,8 @@
 import os
 
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 
 """
@@ -11,3 +13,23 @@ from flask import Flask
 """
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(12).hex()
+
+
+"""
+------------------------------------- DB Setup & Migrate ------------------------------------
+"""
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+os.path.join(basedir, "data.sqlite")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app=app, db=db)
+
+
+"""
+---------------------------------------- Blueprints ------------------------------------------
+"""
+from radzblog.core.views import core
+
+
+app.register_blueprint(core)
